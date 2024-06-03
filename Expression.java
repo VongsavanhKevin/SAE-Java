a98f094f4d6e8e8261d91ebd505ec388fd76a1a8
Kevin <vongkevin704@gmail.com>
2024-06-03 22:46:08 +0200

    Relecture et ajout des commentaires

--- Expression.java
+++ Expression.java
@@ -1,5 +1,13 @@
+/**
+ * Classe abstraite représentant une expression mathématique.
+ * Elle sert de base pour d'autres classes qui doivent implémenter les méthodes abstraites définies ici.
+*/
 public abstract class Expression
 {
+    /**
+     * Méthode abstraite pour obtenir la valeur de l'expression
+     * Cette méthode doit être implémentée par les sous-classes de Expression.
+     * Retourne la valeur numérique de l'expression
+    */
     public abstract double valeur();
-
 }
